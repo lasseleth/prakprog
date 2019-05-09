b1a8951bb8175d5a5b1be98d0d25d95f5f9c4b15
lasseleth <lasseleth.lb@gmail.com>
2019-05-09 13:54:47 +0200

    Added folder num

---
+++ num/interpolation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  7 12:38:42 2019
+
+@author: lasse
+"""
+import numpy as np
+import scipy as sp
+from matplotlib import pyplot as plt
