b0f908879f8bdcb3ec89fe70393623dde031b71f
lasseleth <lasseleth.lb@gmail.com>
2019-05-14 12:02:30 +0200

    Added week 6 exercise ''stdio and gnuplot''

---
+++ prak/ch6/main-cmdline.c
@@ -0,0 +1,14 @@
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char **argv)
+{
+
+for(int i=1;i<argc;i++) {
+	double x=atof(argv[i]);
+	printf("%lg \t %lg\n",x,sin(x));
+}
+
+return 0;
+}
---
+++ prak/ch6/main-stdio.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+int main(){
+
+    double x;
+    while( scanf("%lg",&x) != EOF ) printf("%lg \t %lg\n",x,cos(x));
+
+return 0;
+}
