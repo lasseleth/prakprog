3cebb5825d20c179c01475063d41585e726b8e2e
lasseleth <lasseleth.lb@gmail.com>
2019-05-29 13:37:35 +0200

    Made main and Integration.py, and thus did the first two tasks, as it can do the CC method

---
+++ num/Adaptive_int/Integration.py
@@ -0,0 +1,65 @@
+import math as mt
+import numpy as np
+def ra_integrator(f, a, b, acc, eps, f2, f3, error_int): #Recursive adaptive integrator. Depends on a function, start and end points, accuracy etc. 
+    f1 = f(a + (b -a)/6)
+    f4 = f(a + 5*(b - a)/6)
+    q = (2*(f1 + f4) + f2 + f3)*(b - a)/6
+    Q = (f1 + f2 + f3 + f4)*(b - a)/4
+
+    tol = acc + eps*mt.fabs(Q)
+    error = mt.fabs(Q - q)
+    if (error < tol):
+        error_int = error
+        return Q 
+
+    else:
+        error1 = 0.0
+        error2 = 0.0
+
+        Q1 = ra_integrator(f, a, (a + b)/2, acc/2.0**0.5, eps, f1, f2, error1)
+        Q2 = ra_integrator(f, (a + b)/2, b, acc/2.0**0.5, eps, f3, f4, error2)
+    return (Q1 + Q2) #Result of integration
+
+
+def integrator(f, a, b, acc, eps, error_int): 
+    f2 = f(a + 2*(b - a)/6)
+    f3 = f(a + 4*(b - a)/6)
+
+    Q = ra_integrator(f, a, b, acc, eps, f2, f3, error_int)
+    return Q
+
+
+def inf_integrator(f, a, b, acc, eps, error_int):
+    A = np.isinf(a)
+    B = np.isinf(b)
+
+    if (A == False and B == False):
+        # No infinites
+        Q = integrator(f, a, b, acc, eps, error_int) #No inifinities, works normally
+
+    elif (A == True and B == True): # Both limits are infinite
+        def both_inf(t):
+            return (f(t/(1 - t**2))*(1 + t**2)/(1 - t**2)**2)
+        Q = integrator(both_inf, -1, 1, acc, eps, error_int)
+
+    elif (A == False and B  == True): #Upper bound B is infinite
+        def Up_inf(t):
+            return (f(a + (1 - t)/t)/t**2)
+        Q = integrator(Up_inf, 0, 1, acc, eps, error_int)
+
+    elif (A == True and B == False): #Lower bound A is infinite
+        def Lo_inf(t):
+            return (f(b - (1 - t)/t)/t**2)
+        Q = integrator(Lo_inf, 0, 1, acc, eps, error_int)
+
+    else: #For errors
+        print('ERROR')
+        Q = float('Infinity')
+
+    return Q
+
+def Clenshaw_Curtis(f, a, b, acc, eps, error_int): 
+    def F(t):
+        return (f((a + b)/2.0 + (a - b)*np.cos(t)/2.0)*np.sin(t)*(b - a)/2.0) #func
+
+    return (integrator(F, 0, mt.pi, acc, eps, error_int)) #integrating...
---
+++ num/Adaptive_int/main.py
@@ -0,0 +1,122 @@
+from Integration import integrator, inf_integrator, ra_integrator, Clenshaw_Curtis
+import numpy as np
+import sys
+sys.setrecursionlimit(100000)
+
+#Functions to integrate
+def sqrt(x):
+    global callnum1
+    callnum1 = callnum1 +1
+    return x**0.5
+
+def invsqrt(x):
+    global callnum2
+    callnum2 = callnum2 + 1
+    return 1.0/x**0.5
+
+def logsqrt(x):
+    global callnum3
+    callnum3 = callnum3 + 1
+    return np.log(x)/x**0.5
+
+def testfunc(x):
+    global callnum4
+    callnum4 = callnum4 + 1
+    return 4.0*(1-(1-x)**2)**0.5
+
+print("Integrating function with Recursive Adaptive Integrator (RAI)")
+a = 0.0 #lower bound 
+b = 1.0 #upper bound
+acc = 1e-8 #accuracy
+eps = 1e-8 #relative accuracy
+
+
+#_____________________________________________________________
+#sqrt integ
+callnum1 = 0
+error1 = 0
+print("\n------ Intergrating sqrt from 0 to 1 ------")
+result_sqrt = integrator(sqrt, a, b, acc, eps, error1)
+
+print("\nNumerical solution: ", result_sqrt)
+print("Analytical solution: ", 2.0/3.0)
+print("There were %i calls to the function." % (callnum1) )
+print("The error is estimated to be: ", acc + np.fabs(result_sqrt)*eps) #prints the calculated error
+print("The actual error is: ", np.fabs(result_sqrt- 2.0/3.0))
+print("\n")
+
+#_____________________________________________________________
+#inverse sqrt integ
+callnum2 = 0
+error2 = 0
+print("\n------ Intergrating inverse sqrt from 0 to 1 ------")
+result_invsqrt = integrator(invsqrt, a, b, acc, eps, error2)
+
+print("\nNumerical solution: ", result_invsqrt)
+print("Analytical solution: ", 2.0)
+print("There were %i calls to the function." % (callnum2) )
+print("The error is estimated to be: ", acc + np.fabs(result_invsqrt)*eps) #prints the calculated error
+print("The actual error is: ", np.fabs(result_invsqrt- 2.0))
+print("\n")
+
+#_____________________________________________________________
+#log sqrt integ
+callnum3 = 0
+error3 = 0
+print("\n------ Intergrating ln/sqrt from 0 to 1 ------")
+result_lnsqrt = integrator(logsqrt, a, b, acc, eps, error3)
+
+print("\nNumerical solution: ", result_lnsqrt)
+print("Analytical solution: ", -4.0)
+print("There were %i calls to the function." % (callnum3) )
+print("The error is estimated to be: ", acc + np.fabs(result_lnsqrt)*eps) #prints the calculated error
+print("The actual error is: ", np.fabs(result_lnsqrt+ 4.0))
+print("\n")
+
+#_____________________________________________________________
+#testfunc integ
+callnum4 = 0
+error4 = 0
+print("\n------ Intergrating test function from 0 to 1 ------")
+print("Test function is: 4*sqrt(1-(1-x)^2)")
+result_testfunc = integrator(testfunc, a, b, acc, eps, error4)
+
+print("\nNumerical solution: ", result_testfunc)
+print("Analytical solution: ", np.pi)
+print("There were %i calls to the function." % (callnum4) )
+print("The error is estimated to be: ", acc + np.fabs(result_testfunc)*eps) #prints the calculated error
+print("The actual error is: ", np.fabs(result_testfunc - np.pi))
+print("\n")
+
+
+print("###################################")
+#### Now integrating with the Clenshaw-Curtis variable transformation ###
+print("\nNow integrating with the Clenshaw-Curtis variable transformation")
+
+
+callnum2 = 0
+error2 = 0
+print("\n---- Intergrating inverse sqrt from 0 to 1, using the Clenshaw-Curtis method ----")
+cc_invsqrt = Clenshaw_Curtis(invsqrt, a, b, acc, eps, error2)
+
+print("\nNumerical solution: ", cc_invsqrt)
+print("Analytical solution: ", 2.0)
+print("There were %i calls to the function." % (callnum2) )
+print("The error is estimated to be: ", acc + np.fabs(cc_invsqrt)*eps) #prints the calculated error
+print("The actual error is: ", np.fabs(cc_invsqrt- 2.0))
+print("\n")
+
+#_____________________________________________________________
+#testfunc integ
+callnum4 = 0
+error4 = 0
+print("\n---- Intergrating test function from 0 to 1, using the Clenshaw-Curtis method ----")
+print("Test function is: 4*sqrt(1-(1-x)^2)")
+cc_testfunc = integrator(testfunc, a, b, acc, eps, error4)
+
+print("\nNumerical solution: ", cc_testfunc)
+print("Analytical solution: ", np.pi)
+print("There were %i calls to the function." % (callnum4) )
+print("The error is estimated to be: ", acc + np.fabs(result_testfunc)*eps) #prints the calculated error
+print("The actual error is: ", np.fabs(cc_testfunc - np.pi))
+print("\n")
