556dbc71e8e298b47725b1d7a39042e051369722
lasseleth <lasseleth.lb@gmail.com>
2019-06-05 14:49:39 +0200

    Added tex-files, and fixed plots etc.

--- prak/Big_Exercise/main.c
+++ prak/Big_Exercise/main.c
@@ -27,21 +27,16 @@ double coolshit(int ib) {
     for(int i=0;i<n;i++) for(int j=i;j<n;j++) {
         double x = RND;
         gsl_matrix_set(A,i,j,x);
-        gsl_matrix_set(A,j,i,x);
+        gsl_matrix_set(A,j,i,x); //sets the i-j-th index of the matrix to be X, and the same for the j-i-th index (so the matrix is symmetric)
         }
-    gsl_matrix_memcpy(B,A);
-
+    gsl_matrix_memcpy(B,A); //copies the insides of matrix A, into B
+    //matrix_print(A);
 
     gsl_matrix *V = gsl_matrix_alloc(n,n);
     gsl_vector *e = gsl_vector_alloc(n);
     jacobi(A,e,V); //printf("n=%i, sweeps=%i\n",n,sweeps);
 
-
     if(n<max_print){
-    //fprintf(stderr,"\nRandom symmetric matrix A: \n"); matrix_print(B);
-    //fprintf(stderr,"\nResult of Jacobi diagonalization: \n");
-    //fprintf(stderr, "eigenvalues:\n");
-    //vector_print(e);
     largest =  gsl_vector_get(e,0);
     for(int i=0; i<n; i++){
         if( gsl_vector_get(e, i) > largest) {
@@ -49,26 +44,62 @@ double coolshit(int ib) {
         }
         else{largest = largest;}    
     }
-    //fprintf(stderr, "\nOf which %g is the largest\n\n" , largest);
 
     gsl_blas_dgemm(CblasNoTrans,CblasNoTrans,1,B,V,0,A);
     gsl_blas_dgemm(CblasTrans  ,CblasNoTrans,1,V,A,0,B);
-    //fprintf(stderr, "check: V^T*A*V should be diagonal with above eigenvalues:\n");
-    //matrix_print(B);
     }
-    //fprintf(stderr, "If you wish to change the size of the matrix, make clean and the type make n=>>value<<\n");
     return largest;
 }
 
-int main(int argc, char** argv)
-{
-int n = (argc>1? atoi(argv[1]):5);
-//printf("\n");
-for(int i=1; i<n+1; i++) {
-    double eign = coolshit(i);
-    printf("%d  %g\n" , i, eign);
-}
-//printf("\n");
+#define REAL double
+inline static REAL sqr(REAL x) {return x*x;}
+int linreg(int n, const REAL x[], const REAL y[], REAL* m, REAL* b, REAL* r){
+    REAL   sumx = 0.0;                      /* sum of x     */
+    REAL   sumx2 = 0.0;                     /* sum of x**2  */
+    REAL   sumxy = 0.0;                     /* sum of x * y */
+    REAL   sumy = 0.0;                      /* sum of y     */
+    REAL   sumy2 = 0.0;                     /* sum of y**2  */
 
-return 0;
+    for (int i=0;i<n;i++){ 
+        sumx  += x[i];       
+        sumx2 += sqr(x[i]);  
+        sumxy += x[i] * y[i];
+        sumy  += y[i];      
+        sumy2 += sqr(y[i]); } 
+    REAL denom = (n * sumx2 - sqr(sumx));
+    if (denom == 0) {
+        // singular matrix. can't solve the problem.
+        *m = 0;
+        *b = 0;
+        if (r) *r = 0;
+            return 1;}
+    *m = (n * sumxy  -  sumx * sumy) / denom;
+    *b = (sumy * sumx2  -  sumx * sumxy) / denom;
+    if (r!=NULL) {
+        *r = (sumxy - sumx * sumy / n) /    /* compute correlation coeff */
+              sqr((sumx2 - sqr(sumx)/n) *
+              (sumy2 - sqr(sumy)/n));}
+    return 0; }
+
+
+int main(int argc, char** argv){
+    
+    int n = (argc>1? atoi(argv[1]):5);
+    double x [n];
+    double y [n];
+
+    for(int i=1; i<n+1; i++) {
+        double eign = coolshit(i);
+        //printf("%d  %g\n" , i, eign);
+        x[i] = i;
+        y[i] = eign;
+    }
+
+    REAL a,b,r;
+        linreg(n,x,y,&a,&b,&r);
+        for(int i=1; i<n+1; i++){
+            printf("%g  %g  %g  %g\n",x[i], y[i], a, b); //x is N-size, y is largest eigenvalue, a is slope og the fit, and b is crosspoint with y-axis of the fit  
+        }
+
+    return 0;
 }
