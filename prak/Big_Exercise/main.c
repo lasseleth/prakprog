7dcc1866685614cb956c80c317a040a2958fa646
lasseleth <lasseleth.lb@gmail.com>
2019-06-05 10:47:12 +0200

    Fixed the stuff, so now the script gives a list of the largest eigenvalues, for matrices of size n*n

--- prak/Big_Exercise/main.c
+++ prak/Big_Exercise/main.c
@@ -4,7 +4,7 @@
 #include<gsl/gsl_blas.h>
 #define RND ((double)rand()/RAND_MAX)
 #define FMT "%7.3f"
-#define max_print 11
+#define max_print 52
 
 int jacobi(gsl_matrix*A,gsl_vector*e,gsl_matrix*V);
 
@@ -18,44 +18,57 @@ void vector_print(gsl_vector *v){
 	fprintf(stderr,"\n");
 	}
 
+double coolshit(int ib) {
+    int n= ib;
+    double largest = 0;
+
+    gsl_matrix *A = gsl_matrix_alloc(n,n);
+    gsl_matrix *B = gsl_matrix_alloc(n,n);
+    for(int i=0;i<n;i++) for(int j=i;j<n;j++) {
+        double x = RND;
+        gsl_matrix_set(A,i,j,x);
+        gsl_matrix_set(A,j,i,x);
+        }
+    gsl_matrix_memcpy(B,A);
+
+
+    gsl_matrix *V = gsl_matrix_alloc(n,n);
+    gsl_vector *e = gsl_vector_alloc(n);
+    jacobi(A,e,V); //printf("n=%i, sweeps=%i\n",n,sweeps);
+
+
+    if(n<max_print){
+    //fprintf(stderr,"\nRandom symmetric matrix A: \n"); matrix_print(B);
+    //fprintf(stderr,"\nResult of Jacobi diagonalization: \n");
+    //fprintf(stderr, "eigenvalues:\n");
+    //vector_print(e);
+    largest =  gsl_vector_get(e,0);
+    for(int i=0; i<n; i++){
+        if( gsl_vector_get(e, i) > largest) {
+            largest = gsl_vector_get(e, i);
+        }
+        else{largest = largest;}    
+    }
+    //fprintf(stderr, "\nOf which %g is the largest\n\n" , largest);
+
+    gsl_blas_dgemm(CblasNoTrans,CblasNoTrans,1,B,V,0,A);
+    gsl_blas_dgemm(CblasTrans  ,CblasNoTrans,1,V,A,0,B);
+    //fprintf(stderr, "check: V^T*A*V should be diagonal with above eigenvalues:\n");
+    //matrix_print(B);
+    }
+    //fprintf(stderr, "If you wish to change the size of the matrix, make clean and the type make n=>>value<<\n");
+    return largest;
+}
+
 int main(int argc, char** argv)
 {
-int n=(argc>1? atoi(argv[1]):5);
-
-gsl_matrix *A = gsl_matrix_alloc(n,n);
-gsl_matrix *B = gsl_matrix_alloc(n,n);
-for(int i=0;i<n;i++) for(int j=i;j<n;j++) {
-	double x = RND;
-	gsl_matrix_set(A,i,j,x);
-	gsl_matrix_set(A,j,i,x);
-	}
-gsl_matrix_memcpy(B,A);
-
-
-gsl_matrix *V = gsl_matrix_alloc(n,n);
-gsl_vector *e = gsl_vector_alloc(n);
-int sweeps=jacobi(A,e,V); printf("n=%i, sweeps=%i\n",n,sweeps);
-
-
-if(n<max_print){
-fprintf(stderr,"\nRandom symmetric matrix A: \n"); matrix_print(B);
-fprintf(stderr,"\nResult of Jacobi diagonalization: \n");
-fprintf(stderr, "eigenvalues:\n");
-vector_print(e);
-double largest =  gsl_vector_get(e,0);
-for(int i=0; i<n; i++){
-    if( gsl_vector_get(e, i) > largest) {
-        largest = gsl_vector_get(e, i);
-    }
-    else{largest = largest;}    
+int n = (argc>1? atoi(argv[1]):5);
+printf("\n");
+for(int i=1; i<n+1; i++) {
+    double eign = coolshit(i);
+    printf("%g\n" , eign);
 }
-fprintf(stderr, "\nOf which %g is the largest\n\n" , largest);
+printf("\n");
 
-gsl_blas_dgemm(CblasNoTrans,CblasNoTrans,1,B,V,0,A);
-gsl_blas_dgemm(CblasTrans  ,CblasNoTrans,1,V,A,0,B);
-//fprintf(stderr, "check: V^T*A*V should be diagonal with above eigenvalues:\n");
-//matrix_print(B);
-}
-fprintf(stderr, "If you wish to change the size of the matrix, make clean and the type make n=>>value<<\n");
 return 0;
 }
